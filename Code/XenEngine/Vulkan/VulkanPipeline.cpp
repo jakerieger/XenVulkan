96394630a10eb86f291d819bbc8443dac4767420
Jake Rieger <contact.jakerieger@gmail.com>
2025-01-09 20:10:26 -0500

    vulkan pipeline container class

--- Code/XenEngine/Vulkan/VulkanPipeline.cpp
+++ Code/XenEngine/Vulkan/VulkanPipeline.cpp
@@ -5,4 +5,51 @@
 #include "VulkanPipeline.hpp"
 
 namespace x::vk {
-} // x
+    VulkanPipeline::VulkanPipeline(VkDevice device)
+        : _device(device), _pipeline(None), _layout(None) {}
+
+    VulkanPipeline::~VulkanPipeline() {
+        Cleanup();
+    }
+
+    VkPipeline VulkanPipeline::Handle() const {
+        return _pipeline;
+    }
+
+    void VulkanPipeline::Cleanup() {
+        if (_device) {
+            if (_pipeline) {
+                vkDestroyPipeline(_device, _pipeline, None);
+                _pipeline = None;
+            }
+            if (_layout) {
+                vkDestroyPipelineLayout(_device, _layout, None);
+                _layout = None;
+            }
+        }
+    }
+
+    VulkanPipeline::VulkanPipeline(VulkanPipeline&& other) noexcept
+        : _device(other._device), _pipeline(other._pipeline), _layout(other._layout) {
+        other._device   = None;
+        other._pipeline = None;
+        other._layout   = None;
+    }
+
+    VulkanPipeline& VulkanPipeline::operator=(VulkanPipeline&& other) noexcept {
+        if (this != &other) {
+            Cleanup();
+            _device         = other._device;
+            _pipeline       = other._pipeline;
+            _layout         = other._layout;
+            other._device   = None;
+            other._pipeline = None;
+            other._layout   = None;
+        }
+        return *this;
+    }
+
+    void VulkanPipeline::Bind(VkCommandBuffer commandBuffer, VkPipelineBindPoint bindPoint) const {
+        if (commandBuffer && _pipeline) { vkCmdBindPipeline(commandBuffer, bindPoint, _pipeline); }
+    }
+}  // namespace x::vk
--- Code/XenEngine/Vulkan/VulkanPipeline.hpp
+++ Code/XenEngine/Vulkan/VulkanPipeline.hpp
@@ -4,10 +4,39 @@
 
 #pragma once
 
+#include <vector>
+#include <vulkan/vulkan_core.h>
+
+#include "Types.hpp"
+#include "Panic.inl"
+
 namespace x::vk {
+    class VulkanPipeline {
+        friend class VulkanPipelineBuilder;
 
-class VulkanPipeline {
+    public:
+        explicit VulkanPipeline(VkDevice device);
 
-};
+        ~VulkanPipeline();
 
-} // x
+        VulkanPipeline(const VulkanPipeline&)            = delete;
+        VulkanPipeline& operator=(const VulkanPipeline&) = delete;
+
+        VulkanPipeline(VulkanPipeline&& other) noexcept;
+        VulkanPipeline& operator=(VulkanPipeline&& other) noexcept;
+
+        // Bind the pipeline to a command buffer
+        void Bind(VkCommandBuffer commandBuffer,
+                  VkPipelineBindPoint bindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS) const;
+
+        // Getter for the internal pipeline handle
+        [[nodiscard]] VkPipeline Handle() const;
+
+    private:
+        VkDevice _device;
+        VkPipeline _pipeline;
+        VkPipelineLayout _layout;
+
+        void Cleanup();
+    };
+}  // namespace x::vk
